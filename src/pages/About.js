d1a704c1b8d00de8788096226c1d57f886130964
Dimitar <dimitar.mandarliev@gmail.com>
2023-01-19 18:18:05 +0200

    Added the final functionality

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Urna id volutpat
+        lacus laoreet non. Blandit volutpat maecenas volutpat blandit aliquam
+        etiam erat velit. At ultrices mi tempus imperdiet nulla malesuada
+        pellentesque elit. Eleifend donec pretium vulputate sapien. Id venenatis
+        a condimentum vitae sapien pellentesque. Scelerisque varius morbi enim
+        nunc faucibus a pellentesque. Amet nisl suscipit adipiscing bibendum est
+        ultricies integer quis auctor. In eu mi bibendum neque egestas congue
+        quisque egestas diam. Cursus vitae congue mauris rhoncus. Morbi tempus
+        iaculis urna id volutpat lacus. Ornare arcu odio ut sem nulla pharetra
+        diam. Posuere urna nec tincidunt praesent semper. Tempor orci dapibus
+        ultrices in iaculis nunc. Nibh venenatis cras sed felis eget velit
+        aliquet sagittis id. Commodo ullamcorper a lacus vestibulum sed arcu non
+        odio euismod. Accumsan in nisl nisi scelerisque eu ultrices.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
